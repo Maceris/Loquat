d32ee825439015c2d7e08ac8db9fe42739af5651
David Burks <chesburks@gmail.com>
2023-12-31 17:50:19 -0500

    Move logic out of entry point, slightly

--- src/main/loquat.cpp
+++ src/main/loquat.cpp
@@ -9,28 +9,41 @@
 
 GlobalState* g_global_state = new GlobalState();
 
+namespace loquat
+{
+	int main();
+}
+
 int main(int argc, char* argv[])
 {
-	Logger::init();
-	Logger::set_display_flags("Debug", FLAG_WRITE_TO_DEBUGGER);
+	return loquat::main();
+}
 
-	glfwInit();
-	if (!glfwVulkanSupported())
+namespace loquat
+{
+	int main()
 	{
-		LOG_FATAL("Vulkan is not supported on this system!");
+		Logger::init();
+		Logger::set_display_flags("Debug", FLAG_WRITE_TO_DEBUGGER);
+
+		glfwInit();
+		if (!glfwVulkanSupported())
+		{
+			LOG_FATAL("Vulkan is not supported on this system!");
+		}
+
+		create_vulkan_instance();
+		create_vulkan_window();
+		g_global_state->device = new Device();
+
+		while (!g_global_state->window_state->window->should_close())
+		{
+			glfwPollEvents();
+		}
+
+		delete g_global_state;
+		glfwTerminate();
+		Logger::destroy();
+		return 0;
 	}
-
-	create_vulkan_instance();
-	create_vulkan_window();
-	g_global_state->device = new Device();
-
-	while (!g_global_state->window_state->window->should_close())
-	{
-		glfwPollEvents();
-	}
-
-	delete g_global_state;
-	glfwTerminate();
-	Logger::destroy();
-	return 0;
 }
