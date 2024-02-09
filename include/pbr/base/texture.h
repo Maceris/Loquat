1634176024f671200c5306ffdc91d65ade98c74f
David Burks <chesburks@gmail.com>
2024-02-08 20:12:02 -0500

    Add base texture declarations

--- include/pbr/base/texture.h
+++ include/pbr/base/texture.h
@@ -6,42 +6,87 @@
 
 #pragma once
 
+#include "main/loquat.h"
+#include "pbr/util/tagged_pointer.h"
+
+#include <string_view>
+
 namespace loquat
 {
 	struct TextureEvalContext;
 
+	class FloatConstantTexture;
+	class FloatBilerpTexture;
+	class FloatCheckerboardTexture;
+	class FloatDotsTexture;
 	/// <summary>
 	/// Fractional Brownian Motion noise.
 	/// </summary>
 	class FBMTexture;
-	class FloatBilerpTexture;
-	class FloatCheckerboardTexture;
-	class FloatConstantTexture;
-	class FloatDirectionMixTexture;
-	class FloatDotsTexture;
+	class GPUFloatImageTexture;
 	class FloatImageTexture;
 	class FloatMixTexture;
+	class FloatDirectionMixTexture;
 	class FloatPtexTexture;
-	class FloatScaledTexture;
-	class GPUFloatImageTexture;
 	class GPUFloatPtexTexture;
-	class GPUSpectrumImageTexture;
-	class GPUSpectrumPtexTexture;
-	class MarbleTexture;
-	class RGBConstantTexture;
-	class RGBReflectanceConstantTexture;
-	class SpectrumBilerpTexture;
-	class SpectrumCheckerboardTexture;
-	class SpectrumConstantTexture;
-	class SpectrumDirectionMixTexture;
-	class SpectrumDotsTexture;
-	class SpectrumImageTexture;
-	class SpectrumMixTexture;
-	class SpectrumPtexTexture;
-	class SpectrumScaledTexture;
+	class FloatScaledTexture;
 	class WindyTexture;
 	class WrinkledTexture;
 
-	class FloatTexture;
-	class SpectrumTexture;
+	class FloatTexture : public TaggedPointer<FloatImageTexture,
+		GPUFloatImageTexture, FloatMixTexture, FloatDirectionMixTexture,
+		FloatScaledTexture, FloatConstantTexture, FloatBilerpTexture,
+		FloatCheckerboardTexture, FloatDotsTexture, FBMTexture,
+		FloatPtexTexture, GPUFloatPtexTexture, WindyTexture, WrinkledTexture>
+	{
+	public:
+		using TaggedPointer::TaggedPointer;
+
+		static FloatTexture create(std::string_view name,
+			const Transform& render_from_texture,
+			const TextureParameterDictionary& parameters,
+			Allocator allocator, bool gpu) noexcept;
+
+		[[nodiscard]]
+		std::string to_string() const noexcept;
+
+		inline Float evaluate(TextureEvalContext context) const noexcept;
+	};
+	
+	class RGBConstantTexture;
+	class RGBReflectanceConstantTexture;
+	class SpectrumConstantTexture;
+	class SpectrumBilerpTexture;
+	class SpectrumCheckerboardTexture;
+	class SpectrumImageTexture;
+	class GPUSpectrumImageTexture;
+	class MarbleTexture;
+	class SpectrumMixTexture;
+	class SpectrumDirectionMixTexture;
+	class SpectrumDotsTexture;
+	class SpectrumPtexTexture;
+	class GPUSpectrumPtexTexture;
+	class SpectrumScaledTexture;
+
+	class SpectrumTexture : public TaggedPointer<SpectrumImageTexture,
+		GPUSpectrumImageTexture, SpectrumMixTexture,
+		SpectrumDirectionMixTexture, SpectrumScaledTexture,
+		SpectrumConstantTexture, SpectrumBilerpTexture,
+		SpectrumCheckerboardTexture, MarbleTexture, SpectrumDotsTexture,
+		SpectrumPtexTexture, GPUSpectrumPtexTexture>
+	{
+	public:
+		using TaggedPointer::TaggedPointer;
+
+		static SpectrumTexture create(std::string_view name,
+			const Transform& render_from_texture,
+			const TextureParameterDictionary& parameters,
+			SpectrumType spectrum_type, Allocator allocator, bool gpu)
+			noexcept;
+
+		[[nodiscard]]
+		std::string to_string() const noexcept;
+
+		inline Float evaluate(TextureEvalContext context) const noexcept;
+	};
 }
