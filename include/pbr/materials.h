d27264367bf181b45b6d1f0ad5ad737af0378fdc
David Burks <chesburks@gmail.com>
2024-05-01 21:29:38 -0400

    Start on interaction class to support material

--- include/pbr/materials.h
+++ include/pbr/materials.h
@@ -7,8 +7,29 @@
 #pragma once
 
 #include "main/loquat.h"
+#include "pbr/interaction.h"
+#include "pbr/base/material.h"
 
 namespace loquat
 {
 
+    struct MaterialEvalContext : public TextureEvalContext
+    {
+        MaterialEvalContext() = default;
+        MaterialEvalContext(const SurfaceInteraction& interaction)
+            : TextureEvalContext{si}
+            , outgoing{interaction.wo}
+            , normal(interaction.shading.n)
+            , dpdu(interaction.shading.dpdu)
+        {}
+        
+        [[nodiscard]]
+        std::string to_string() const noexcept;
+        
+        Vector3f outgoing;
+        Normal3f normal;
+        Vector3f dpdu;
+    }
+
+//TODO(ches) finish this
 }
