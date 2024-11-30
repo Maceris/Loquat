634d29e4d014b3ac4c66a2c6afa91a6ae986218c
David Burks <chesburks@gmail.com>
2024-11-30 18:04:49 -0500

    Add optional

--- include/pbr/util/pstd.h
+++ include/pbr/util/pstd.h
@@ -247,5 +247,209 @@ namespace pstd
     private:
         T values[N] = {};
     };
+
+
+    template <typename T>
+    class optional
+    {
+    public:
+        using value_type = T;
+
+        optional() = default;
+
+        LOQUAT_CPU_GPU
+        optional(const T& v)
+            : set(true)
+        {
+            new (ptr()) T(v);
+        }
+
+        LOQUAT_CPU_GPU
+        optional(T&& v)
+            : set(true)
+        {
+            new (ptr()) T(std::move(v));
+        }
+
+        LOQUAT_CPU_GPU
+        optional(const optional& v)
+            : set(v.has_value())
+        {
+            if (v.has_value())
+            {
+                new (ptr()) T(v.value());
+            }
+        }
+
+        LOQUAT_CPU_GPU
+        optional(optional&& v)
+            : set(v.has_value())
+        {
+            if (v.has_value())
+            {
+                new (ptr()) T(std::move(v.value()));
+                v.reset();
+            }
+        }
+
+        LOQUAT_CPU_GPU
+        optional& operator=(const T& v)
+        {
+            reset();
+            new (ptr()) T(v);
+            set = true;
+            return *this;
+        }
+
+        LOQUAT_CPU_GPU
+        optional& operator=(T&& v)
+        {
+            reset();
+            new (ptr()) T(std::move(v));
+            set = true;
+            return *this;
+        }
+
+        LOQUAT_CPU_GPU
+        optional& operator=(const optional& v)
+        {
+            reset();
+            if (v.has_value()) {
+                new (ptr()) T(v.value());
+                set = true;
+            }
+            return *this;
+        }
+
+        LOQUAT_CPU_GPU
+        optional& operator=(optional&& v)
+        {
+            reset();
+            if (v.has_value()) {
+                new (ptr()) T(std::move(v.value()));
+                set = true;
+                v.reset();
+            }
+            return *this;
+        }
+
+        LOQUAT_CPU_GPU
+        ~optional()
+        {
+            reset();
+        }
+
+        LOQUAT_CPU_GPU
+        explicit operator bool() const
+        {
+            return set;
+        }
+
+        LOQUAT_CPU_GPU
+        T value_or(const T& alt) const
+        {
+            return set ? value() : alt;
+        }
+
+        LOQUAT_CPU_GPU
+        T* operator->()
+        {
+            return &value();
+        }
+
+        LOQUAT_CPU_GPU
+        const T* operator->() const
+        {
+            return &value();
+        }
+
+        LOQUAT_CPU_GPU
+        T& operator*()
+        {
+            return value();
+        }
+
+        LOQUAT_CPU_GPU
+        const T& operator*() const
+        {
+            return value();
+        }
+
+        LOQUAT_CPU_GPU
+        T& value()
+        {
+            LOG_ASSERT(set);
+            return *ptr();
+        }
+
+        LOQUAT_CPU_GPU
+        const T& value() const
+        {
+            LOG_ASSERT(set);
+            return *ptr();
+        }
+
+        LOQUAT_CPU_GPU
+        void reset()
+        {
+            if (set)
+            {
+                value().~T();
+                set = false;
+            }
+        }
+
+        LOQUAT_CPU_GPU
+        bool has_value() const
+        {
+            return set;
+        }
+
+    private:
+#ifdef __NVCC__
+        // Work around NVCC bug
+        LOQUAT_CPU_GPU
+        T* ptr()
+        {
+            return reinterpret_cast<T*>(&optionalValue);
+        }
+
+        LOQUAT_CPU_GPU
+        const T* ptr() const
+        {
+            return reinterpret_cast<const T*>(&optionalValue);
+        }
+#else
+        LOQUAT_CPU_GPU
+        T* ptr()
+        {
+            return std::launder(reinterpret_cast<T*>(&optionalValue));
+        }
+
+        LOQUAT_CPU_GPU
+        const T* ptr() const
+        {
+            return std::launder(reinterpret_cast<const T*>(&optionalValue));
+        }
+#endif
+
+        std::aligned_storage_t<sizeof(T), alignof(T)> optionalValue;
+        bool set = false;
+    };
+
+    template <typename T>
+    inline std::ostream& operator<<(std::ostream& os, const optional<T>& opt)
+    {
+        if (opt.has_value())
+        {
+            return os << "[ pstd::optional<" << typeid(T).name() <<
+                "> set: true " << "value: " << opt.value() << " ]";
+        }
+        else
+        {
+            return os << "[ pstd::optional<" << typeid(T).name()
+                << "> set: false value: n/a ]";
+        }
+    }
 }
 //TODO(ches) finish this
