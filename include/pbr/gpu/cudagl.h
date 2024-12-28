6eb1d8acc30ce7a4b70520831fa40a6961ff7cca
David Burks <chesburks@gmail.com>
2024-12-27 19:45:14 -0500

    Add gui header contents

--- include/pbr/gpu/cudagl.h
+++ include/pbr/gpu/cudagl.h
@@ -7,3 +7,54 @@
 #pragma once
 
 //TODO(ches) fill this out
+
+template <typename PIXEL_FORMAT>
+class CUDAOutputBuffer
+{
+public:
+    CUDAOutputBuffer(int32_t width, int32_t height);
+    ~CUDAOutputBuffer();
+
+    void start_asynchronous_readback();
+    const PIXEL_FORMAT* get_readback_pixels();
+
+    void draw(int window_width, int window_height)
+    {
+        display->display(m_width, m_height, window_width, window_height, get_PBO());
+    }
+
+    PIXEL_FORMAT* map();
+    void unmap();
+
+private:
+    void setStream(CUstream stream)
+    {
+        m_stream = stream;
+    }
+
+    // Allocate or update device pointer as necessary for CUDA access
+    void make_current()
+    {
+        CUDA_CHECK(cudaSetDevice(m_device_idx));
+    }
+
+    // Get output buffer
+    GLuint get_PBO();
+    void deletePBO();
+
+    int32_t m_width = 0u;
+    int32_t m_height = 0u;
+
+    cudaGraphicsResource* m_cuda_gfx_resource = nullptr;
+    GLuint m_pbo = 0u;
+    PIXEL_FORMAT* m_device_pixels = nullptr;
+    PIXEL_FORMAT* m_host_pixels = nullptr;
+
+    bool readback_active = false;
+    cudaEvent_t readback_finished_event;
+
+    CUstream m_stream = 0u;
+    int32_t m_device_idx = 0;
+
+    BufferDisplay* display = nullptr;
+};
--- include/pbr/util/gui.h
+++ include/pbr/util/gui.h
@@ -7,3 +7,100 @@
 #pragma once
 
 //TODO(ches) fill this out
+
+#include <set>
+#include <string>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "main/loquat.h"
+
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+#include "pbr/gpu/cudagl.h"
+#endif
+
+#include "pbr/util/color.h"
+#include "pbr/math/transform.h"
+#include "pbr/math/vector_math.h"
+
+namespace loquat
+{
+    enum DisplayState
+    {
+        EXIT,
+        RESET,
+        NONE
+    };
+
+    class GUI
+    {
+    public:
+        GUI(std::string title, Vec2i resolution, AABB3f sceneBounds);
+        ~GUI();
+
+        RGB* map_framebuffer()
+        {
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+            if (cuda_framebuffer)
+            {
+                return cuda_framebuffer->map();
+            }
+            else
+#endif
+            {
+                return cpu_framebuffer;
+            }
+        }
+
+        void unmap_framebuffer()
+        {
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+            if (cuda_framebuffer)
+            {
+                cuda_framebuffer->unmap();
+            }
+#endif
+        }
+
+        DisplayState refresh_display();
+
+        // It's a little messy that the state of values controlled via the UI
+        // are just public variables here but it's probably not worth putting
+        // an abstraction layer on top of all this at this point.
+        Transform get_camera_transform() const { return moving_from_camera; }
+        Float exposure = 1.f;
+        bool print_camera_transform = false;
+
+        void keyboard_callback(GLFWwindow* window, int key, int scan, int action, int mods);
+        void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos);
+        void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+
+        static void initialize();
+        static Point2i get_resolution();
+
+    private:
+        bool process_keys();
+        bool process_mouse();
+        bool process();
+
+        std::set<char> keys_down;
+        Float move_scale = 1.f;
+        Transform moving_from_camera;
+        Vec2i resolution;
+        bool record_frames = false;
+        int frame_number = 0;
+        bool pressed = false;
+        Float xoffset = 0.f;
+        Float yoffset = 0.f;
+        double lastX = 0.f;
+        double lastY = 0.f;
+
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+        CUDAOutputBuffer<RGB>* cuda_framebuffer = nullptr;
+#endif
+        RGB* cpu_framebuffer = nullptr;
+        GLFWwindow* window = nullptr;
+    };
+
+}
