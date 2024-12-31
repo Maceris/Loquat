63247dbf73b526eb56f725630d5e2ce988b70b1d
David Burks <chesburks@gmail.com>
2024-12-31 18:13:08 -0500

    Fill out noise header

--- include/pbr/util/noise.h
+++ include/pbr/util/noise.h
@@ -6,4 +6,18 @@
 
 #pragma once
 
-//TODO(ches) fill this out
+#include "main/loquat.h"
+
+namespace loquat
+{
+	LOQUAT_CPU_GPU
+	Float noise(Float x, Float y = .5f, Float z = .5f);
+	LOQUAT_CPU_GPU
+	Float noise(Point3f p);
+	LOQUAT_CPU_GPU
+	Vec3f d_noise(Point3f p);
+	LOQUAT_CPU_GPU
+	Float fbm(Point3f p, Vec3f dpdx, Vec3f dpdy, Float omega, int octaves);
+	LOQUAT_CPU_GPU
+	Float turbulence(Point3f p, Vec3f dpdx, Vec3f dpdy, Float omega, int octaves);
+}
