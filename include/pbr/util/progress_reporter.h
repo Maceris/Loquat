283b0751baa73ad8932b12193866197f8e8e3979
David Burks <chesburks@gmail.com>
2025-01-01 21:33:24 -0500

    Add progress_reporter header contents

--- include/pbr/util/progress_reporter.h
+++ include/pbr/util/progress_reporter.h
@@ -7,3 +7,109 @@
 #pragma once
 
 //TODO(ches) fill this out
+
+#include <atomic>
+#include <chrono>
+#include <cstdint>
+#include <string>
+#include <thread>
+
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+#include <cuda_runtime.h>
+#include <vector>
+#endif
+
+#include "main/loquat.h"
+
+#include "pbr/util/pstd.h"
+
+namespace loquat
+{
+    class Timer
+    {
+    public:
+        Timer()
+        {
+            start = clock::now();
+        }
+
+        double elapsed_seconds() const
+        {
+            clock::time_point now = clock::now();
+            int64_t elapsed_uS =
+                std::chrono::duration_cast<std::chrono::microseconds>(now - start).count();
+            return elapsed_uS / 1'000'000.0;
+        }
+
+        std::string to_string() const;
+
+    private:
+        using clock = std::chrono::steady_clock;
+        clock::time_point start;
+    };
+
+    class ProgressReporter
+    {
+    public:
+        ProgressReporter()
+            : quiet(true)
+        {}
+        ProgressReporter(int64_t total_work, std::string title, bool quiet,
+            bool gpu = false);
+
+        ~ProgressReporter();
+
+        void update(int64_t num = 1);
+        void done();
+        double elapsed_seconds() const;
+
+        std::string to_string() const;
+
+    private:
+        void print_bar();
+
+        int64_t total_work;
+        std::string title;
+        bool quiet;
+        Timer timer;
+        std::atomic<int64_t> work_done;
+        std::atomic<bool> exit_thread;
+        std::thread update_thread;
+        pstd::optional<float> finish_time;
+
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+        std::vector<cudaEvent_t> gpu_events;
+        std::atomic<size_t> gpu_events_launched_offset;
+        int gpu_events_finished_offset;
+#endif
+    };
+
+    inline double ProgressReporter::elapsed_seconds() const
+    {
+        return finish_time ? *finish_time : timer.elapsed_seconds();
+    }
+
+    inline void ProgressReporter::update(int64_t num)
+    {
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+        if (gpu_events.size() > 0)
+        {
+            if (gpu_events_launched_offset + num <= gpu_events.size())
+            {
+                while (num-- > 0)
+                {
+                    LOG_ASSERT(cudaEventRecord(gpu_events[gpu_events_launched_offset]) == cudaSuccess);
+                    ++gpu_events_launched_offset;
+                }
+            }
+            return;
+        }
+#endif
+        if (num == 0 || quiet)
+        {
+            return;
+        }
+        work_done += num;
+    }
+
+}
