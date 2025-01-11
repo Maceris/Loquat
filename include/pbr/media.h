b523cee3b0da7b4647e18ecb0df93bd98f2c76d8
David Burks <chesburks@gmail.com>
2025-01-10 23:13:00 -0500

    Add third party module builds

--- include/pbr/media.h
+++ include/pbr/media.h
@@ -7,3 +7,29 @@
 #pragma once
 
 //TODO(ches) fill this out
+#include <algorithm>
+#include <limits>
+#include <memory>
+#include <vector>
+
+#include "main/loquat.h"
+
+#include "pbr/textures.h"
+#include "pbr/base/medium.h"
+#include "pbr/math/transform.h"
+#include "pbr/struct/interaction.h"
+#include "pbr/struct/parameter_dictionary.h"
+#include "pbr/util/color_space.h"
+#include "pbr/util/memory.h"
+#include "pbr/util/parallel.h"
+#include "pbr/util/pstd.h"
+#include "pbr/util/scattering.h"
+#include "pbr/util/spectrum.h"
+
+#include "nanovdb/NanoVDB.h"
+#include "nanovdb/util/GridHandle.h"
+#include "nanovdb/util/SampleFromVoxels.h"
+#if defined(LOQUAT_BUILD_GPU_RENDERER) && defined(__NVCC__)
+#include "nanovdb/util/CudaDeviceBuffer.h"
+#endif
+
