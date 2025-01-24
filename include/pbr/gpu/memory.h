0b7d4001ffd1e37bfb3c87a7f12f640a1ac36d51
David Burks <chesburks@gmail.com>
2025-01-24 17:01:16 -0500

    Fill out memory header

--- include/pbr/gpu/memory.h
+++ include/pbr/gpu/memory.h
@@ -6,4 +6,59 @@
 
 #pragma once
 
-//TODO(ches) fill this out
+#include <atomic>
+#include <cstddef>
+#include <memory>
+#include <mutex>
+#include <type_traits>
+#include <unordered_map>
+
+#include "main/loquat.h"
+
+#include "pbr/math/math.h"
+#include "pbr/util/pstd.h"
+
+namespace loquat
+{
+
+#ifdef LOQUAT_BUILD_GPU_RENDERER
+
+    class CUDAMemoryResource : public pstd::pmr::memory_resource
+    {
+        void* do_allocate(size_t size, size_t alignment);
+        void do_deallocate(void* p, size_t bytes, size_t alignment);
+
+        bool do_is_equal(const memory_resource& other) const noexcept
+        {
+            return this == &other;
+        }
+    };
+
+    class CUDATrackedMemoryResource : public CUDAMemoryResource
+    {
+    public:
+        void* do_allocate(size_t size, size_t alignment);
+        void do_deallocate(void* p, size_t bytes, size_t alignment);
+
+        bool do_is_equal(const memory_resource& other) const noexcept
+        {
+            return this == &other;
+        }
+
+        void prefetch_to_GPU() const;
+        size_t get_bytes_allocated() const
+        {
+            return bytes_allocated;
+        }
+
+        static CUDATrackedMemoryResource singleton;
+
+    private:
+        mutable std::mutex mutex;
+        std::atomic<size_t> bytes_allocated{};
+        std::unordered_map<void*, size_t> allocations;
+    };
+
+#endif
+
+}
