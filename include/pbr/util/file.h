8ca679950194fe7cd2b0a2c0cab5600e55545188
David Burks <chesburks@gmail.com>
2025-02-11 19:39:52 -0500

    Fill out file header

--- include/pbr/util/file.h
+++ include/pbr/util/file.h
@@ -6,4 +6,32 @@
 
 #pragma once
 
-//TODO(ches) fill this out
+#include <string>
+#include <vector>
+
+#include "main/loquat.h"
+
+#include "pbr/util/pstd.h"
+
+namespace loquat
+{
+	std::string read_file_contents(std::string filename);
+	std::string read_decompressed_file_contents(std::string filename);
+	bool write_file_contents(std::string filename, const std::string& contents);
+
+	std::vector<Float> read_float_file(std::string filename);
+
+	bool file_exists(std::string filename);
+	bool remove_file(std::string filename);
+
+	std::string resolve_filename(std::string filename);
+	void set_search_directory(std::string filename);
+
+	bool has_extension(std::string filename, std::string ext);
+	std::string remove_extension(std::string filename);
+
+	std::vector<std::string> matching_filenames(std::string filename);
+
+	FILE* fopen_read(std::string filename);
+	FILE* fopen_write(std::string filename);
+}
