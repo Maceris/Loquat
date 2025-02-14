82729eb350c6d9d1fdd601fd157da6f07195b4b1
David Burks <chesburks@gmail.com>
2025-02-13 20:35:57 -0500

    Fill out aggregate header

--- include/pbr/wavefront/aggregate.h
+++ include/pbr/wavefront/aggregate.h
@@ -1,12 +1,11 @@
-// pbrt is Copyright(c) 1998-2020 Matt Pharr, Wenzel Jakob, and Greg Humphreys.
-// The pbrt source code is licensed under the Apache License, Version 2.0.
+// loquat is Copyright(c) 1998-2020 Matt Pharr, Wenzel Jakob, and Greg Humphreys.
+// The loquat source code is licensed under the Apache License, Version 2.0.
 // SPDX: Apache-2.0
 
 // This file has been modified from the original, original notice is above.
 
 #pragma once
 
-//TODO(ches) fill this out
 #include <map>
 #include <string>
 
@@ -22,3 +21,40 @@
 #include "pbr/wavefront/integrator.h"
 #include "pbr/wavefront/work_items.h"
 
+namespace loquat
+{
+    class CPUAggregate : public WavefrontAggregate
+    {
+    public:
+        CPUAggregate(BasicScene& scene, NamedTextures& textures,
+            const std::map<int, pstd::vector<Light>*>& shape_index_to_area_lights,
+            const std::map<std::string, Medium>& media,
+            const std::map<std::string, loquat::Material>& named_materials,
+            const std::vector<loquat::Material>& materials);
+
+        AABB3f get_bounds() const
+        {
+            return aggregate ? aggregate.bounds() : AABB3f();
+        }
+
+        void intersect_closest(int max_rays, const RayQueue* ray_queue,
+            EscapedRayQueue* escaped_ray_queue,
+            HitAreaLightQueue* hit_area_light_queue,
+            MaterialEvalQueue* basic_eval_material_queue,
+            MaterialEvalQueue* universal_eval_material_queue,
+            MediumSampleQueue* medium_sample_queue,
+            RayQueue* next_ray_queue) const;
+
+        void intersect_shadow(int max_rays, ShadowRayQueue* shadow_ray_queue,
+            SOA<PixelSampleState>* pixel_sample_state) const;
+
+        void intersect_shadow_tr(int max_rays, ShadowRayQueue* shadow_ray_queue,
+            SOA<PixelSampleState>* pixel_sample_state) const;
+
+        void intersect_one_random(int max_rays,
+            SubsurfaceScatterQueue* subsurface_scattering_queue) const;
+
+    private:
+        Primitive aggregate;
+    };
+}
