1449dc11a1452d18ea2cadabc84322f531f435d4
David Burks <chesburks@gmail.com>
2025-02-17 21:44:10 -0500

    Fill out optix header

--- include/pbr/gpu/optix/optix.h
+++ include/pbr/gpu/optix/optix.h
@@ -6,4 +6,71 @@
 
 #pragma once
 
-//TODO(ches) fill this out
+#include <optix.h>
+
+#include "main/loquat.h"
+
+#include "pbr/base/light.h"
+#include "pbr/base/material.h"
+#include "pbr/base/medium.h"
+#include "pbr/base/shape.h"
+#include "pbr/base/texture.h"
+#include "pbr/util/pstd.h"
+#include "pbr/wavefront/work_items.h"
+#include "pbr/wavefront/work_queue.h"
+
+namespace loquat
+{
+    class TriangleMesh;
+    class BilinearPatchMesh;
+
+    struct TriangleMeshRecord
+    {
+        const TriangleMesh* mesh;
+        Material material;
+        FloatTexture alpha_texture;
+        pstd::span<Light> area_lights;
+        MediumInterface* medium_interface;
+    };
+
+    struct BilinearMeshRecord
+    {
+        const BilinearPatchMesh* mesh;
+        Material material;
+        FloatTexture alpha_texture;
+        pstd::span<Light> area_lights;
+        MediumInterface* medium_interface;
+    };
+
+    struct QuadricRecord
+    {
+        Shape shape;
+        Material material;
+        FloatTexture alpha_texture;
+        Light area_light;
+        MediumInterface* medium_interface;
+    };
+
+    struct RayIntersectParameters
+    {
+        OptixTraversableHandle traversable;
+
+        const RayQueue* ray_queue;
+
+        // closest hit
+        RayQueue* next_ray_queue;
+        EscapedRayQueue* escaped_ray_queue;
+        HitAreaLightQueue* hit_area_light_queue;
+        MaterialEvalQueue* basic_eval_material_queue;
+        MaterialEvalQueue* universal_eval_material_ueue;
+        MediumSampleQueue* medium_sample_queue;
+
+        // shadow rays
+        ShadowRayQueue* shadow_ray_queue;
+        SOA<PixelSampleState> pixel_sample_state;
+
+        // Subsurface scattering...
+        SubsurfaceScatterQueue* subsurface_scatter_queue;
+    };
+
+}
